8d45cee94d11d169378a2a13d0d72a2498f3231f
maejyomi <141015562+maejyomi@users.noreply.github.com>
2023-10-13 12:11:18 +0900

    Builder 코드
    - @Builder 관련 실습 코드

---
+++ MemberBuilder/src/edu/pnu/BuilderTest.java
@@ -0,0 +1,15 @@
+package edu.pnu;
+
+public class BuilderTest {
+
+	public static void main(String[] args) {
+		Member m = Member.builder()
+				.username("abcd")
+				.password("1234")
+				.build();
+		
+		System.out.println(m);
+
+	}
+
+}
---
+++ MemberBuilder/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module MemberBuilder {
+}
